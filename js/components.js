14744d005ba7433d7cb185e28df85edef06d5172
Jordane865 <jordsomakou865@gmail.com>
2026-05-27 19:10:07 +0100

    v1

--- js/components.js
+++ js/components.js
@@ -85,7 +85,7 @@ const NAVBAR_HTML = `
         </button>
       </div>
 
-      <!-- Language Switcher -->
+      <!-- Language Switcher
       <div class="lang-switcher" id="lang-switcher">
         <button class="lang-btn" id="lang-btn" aria-haspopup="listbox" aria-expanded="false">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="lang-globe"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
@@ -98,7 +98,7 @@ const NAVBAR_HTML = `
           <button class="lang-option" data-lang="es" data-label="ES" role="option">🇪🇸 <span>Español</span></button>
           <button class="lang-option" data-lang="it" data-label="IT" role="option">🇮🇹 <span>Italiano</span></button>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 
